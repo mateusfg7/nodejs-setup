fa64cd07b633638cea002bdf59ce6af0b71e448d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-21 15:33:22 -0300

    update code style based on eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
   return res.json({
-    message: 'Hello Word!',
-  });
-});
+    message: 'Hello Word!'
+  })
+})
 
-app.listen(3333);
+app.listen(3333)
